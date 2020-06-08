69ba65231ca6e82d0f44206572d54625d2194b56
Juan Reséndiz <jresendiz27@gmail.com>
2020-06-08 11:07:23 -0500

    Add terraform backend

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region  = var.aws_region
+}
+resource "aws_s3_bucket" "tfrmstate" {
+  bucket        = "jresendiz27-infrastructure-backend"
+  acl           = "private"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "tf remote state"
+  }
+}
+
+resource "aws_s3_bucket_object" "rmstate_folder" {
+  bucket = aws_s3_bucket.tfrmstate.id
+  key = "terraform-aws/"
+}
+
+resource "aws_dynamodb_table" "terraform_statelock" {
+  name = var.aws_dynamodb_table
+  read_capacity = 20
+  write_capacity = 20
+  hash_key = "LockID"
+
+  attribute {
+      name = "LockID"
+      type = "S"
+  }
+}
